037ea86b9a4d37d0e59cf4a38c136cf38766f792
sumant-007 <sumant@altisec.in>
2024-08-02 12:35:16 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,34 @@
+
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+# check java version:
+JAVA_VERSION=`java -version 2>&1 | awk 'NR==1{ gsub(/"/,""); print $3 }'`
+
+if [[ $JAVA_VERSION == 17* ]]; then
+  echo "Java version ok: $JAVA_VERSION"
+else
+  echo "Java version is expected as 17 but $JAVA_VERSION. try:"
+  echo "export JAVA_HOME=\`/usr/libexec/java_home -v 17\`"
+  exit 1
+fi
+
+# maven build:
+./mvnw -DskipTests clean package
+
+BUILD_IMAGE=0
+
+for i in $@
+do 
+    if [[ $i == "--dockerbuild" ]]; then
+        BUILD_IMAGE=1
+        ./mvnw dockerfile:build
+    fi
+done
+
+if [[ $BUILD_IMAGE == "0" ]]; then
+    echo "Run build.sh --dockerbuild to build docker image."
+fi
+
+echo "DONE"
